836030cbf0856720683d21a018d9998186ccac9a
phoenix1584 <subodh.austin@gmail.com>
2018-04-22 14:12:27 +0530

    #3 : Initial commit. Python version 3.6

---
+++ Python/hello_world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Basic printing
+print (r"Hello World. (Notice not \n needed . Use the r before the double quotes).")
+
+#Some math functions
+
+print ("5*5 = ",5*5)
+print ("5**5 = ",5**5)
